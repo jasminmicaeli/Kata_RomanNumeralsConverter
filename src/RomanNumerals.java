90614bdaab582164156295697ba735557ab574f2
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-08 15:10:38 +0100

    Add special cases (subtraction in roman numerals) for numbers like 4, 10 or 900

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,8 @@
 public class RomanNumerals {
 
     public String convert(int number){
-        String[]        romanNumbers                = {"M", "D", "C", "L", "X", "V", "I"};
-        int[]           correspondingIndianNumerals = {1000, 500, 100, 50, 10, 5, 1};
+        String[]        romanNumbers                = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[]           correspondingIndianNumerals = {1000, 900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1};
         StringBuilder   stringBuilder               = new StringBuilder();
 
         for(int index = 0; index < romanNumbers.length; index++){
