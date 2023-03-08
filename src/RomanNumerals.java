d2387277f672b441226731ef5380b943050957b9
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-08 14:56:45 +0100

    - Add a stringbuilder to append roman numerals to - Add logic to loop through numbers - Add stringbuilder String for return

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,9 +1,15 @@
 public class RomanNumerals {
 
     public String convert(int number){
-        String[]    romanNumbers                = {"M", "D", "C", "L", "X", "V", "I"};
-        int[]       correspondingIndianNumerals = {1000, 500, 100, 50, 10, 5, 1};
+        String[]        romanNumbers                = {"M", "D", "C", "L", "X", "V", "I"};
+        int[]           correspondingIndianNumerals = {1000, 500, 100, 50, 10, 5, 1};
+        StringBuilder   stringBuilder               = new StringBuilder();
 
-        return"";
+        for(int index = 0; index < romanNumbers.length; index++){
+            if(number >= correspondingIndianNumerals[index]){
+                stringBuilder.append(romanNumbers[index]);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
