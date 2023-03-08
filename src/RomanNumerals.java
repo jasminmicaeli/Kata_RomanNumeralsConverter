c123e052d4bcd9ad96719ad97c3e45223274d85e
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-08 14:47:57 +0100

    Add arrays with roman and indian numerals to find corresponding value

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,6 +1,9 @@
 public class RomanNumerals {
 
     public String convert(int number){
-        return "";
+        String[]    romanNumbers                = {"M", "D", "C", "L", "X", "V", "I"};
+        int[]       correspondingIndianNumerals = {1000, 500, 100, 50, 10, 5, 1};
+
+        return"";
     }
 }
