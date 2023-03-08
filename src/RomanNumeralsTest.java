22055887e780890b3ec3797470d1b626ec853ce4
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-08 14:29:24 +0100

    Add a new test for number input 1

--- src/RomanNumeralsTest.java
+++ src/RomanNumeralsTest.java
@@ -4,4 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralsTest {
 
+    @Test
+    public void input_1_shouldReturn_I(){
+        String  expected    = "I";
+        String  actual;
+        int     given       = 1;
+
+        RomanNumerals romanNumerals = new RomanNumerals();
+        actual = romanNumerals.convert(given);
+
+        assertEquals(expected, actual);
+    }
 }
